5cb357670191e0f3f838fe313a378719ad5c52dc
pinkyy <shehiy@student.ethz.ch>
2011-09-22 16:09:15 +0200

    quit

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main(int argc, char** argv) {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_SetVideoMode(800, 600, 0, SDL_HWSURFACE | SDL_DOUBLEBUF);
-	while (true);
+	
+	SDL_Event event;
+	bool keepRunning=true;
+	
+	while (keepRunning) {
+		if (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+				keepRunning = false;
+		}
+	}
 	return 0;
 }
-
 
